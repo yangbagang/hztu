8ea3a7af766d736f1a76b6dc92a6789cbc85f9c2
yangbagang <81667842@qq.com>
2018-04-03 15:27:35 +0800

    fix bugs

--- base/src/main/java/com/ybg/app/base/constants/AppConstant.kt
+++ base/src/main/java/com/ybg/app/base/constants/AppConstant.kt
@@ -8,26 +8,26 @@ import com.ybg.app.base.utils.EnvironmentStateUtil
 object AppConstant {
 
     // 是否首次使用
-    val IS_FIRST_USE = "isFirstUse"
+    const val IS_FIRST_USE = "isFirstUse"
     // 属性文件
-    val PREFERENCE_FILE_NAME = "ybg_hztu_preference.dat"
+    const val PREFERENCE_FILE_NAME = "ybg_hztu_preference.dat"
     // 调试标志
-    val isDebug = false
+    const val isDebug = false
 
     // 外部存储设备的根路径
     private val ExternalStorageRootPath = EnvironmentStateUtil.externalStorageDirectory.path
-    val BasePath = ExternalStorageRootPath + "/hzt_cache/"
+    private val BasePath = "$ExternalStorageRootPath/hzt_cache/"
 
     // 文件存放路径
-    val FILE_CACHE_PATH = BasePath + "fileCache/"
+    val FILE_CACHE_PATH = "${BasePath}fileCache/"
     // 视频存放路径
-    val VIDEO_CACHE_PATH = BasePath + "videoCache/"
+    val VIDEO_CACHE_PATH = "${BasePath}videoCache/"
     //缓存的图片
-    val IMAGE_CACHE_PATH = BasePath + "imageCache/"
+    val IMAGE_CACHE_PATH = "${BasePath}imageCache/"
 
-    val VIDEO_SAVE_PATH = BasePath + "video/"
+    val VIDEO_SAVE_PATH = "${BasePath}video/"
     // 保存图片
-    val IMAGE_SAVE_PATH = BasePath + "photos/"
+    val IMAGE_SAVE_PATH = "${BasePath}photos/"
     // 下载存储地址
-    val DOWNLOAD_PATH = BasePath + "download/"
+    val DOWNLOAD_PATH = "${BasePath}download/"
 }
